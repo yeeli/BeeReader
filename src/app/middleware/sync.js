5a045f86d2909e42a7d25ac7573a11b2365f1121
ye.li <ye.li@mdslife.com>
2018-09-03 09:15:26 +0800

    change sync infomation

--- src/app/middleware/sync.js
+++ src/app/middleware/sync.js
@@ -5,18 +5,19 @@ import api from '~/config/api'
 const sync = opts => store => next => action => {
   const { sync, ...others } = action
   if (!sync) {
-    console.log("sync",action)
     return next(action)
   }
   const { method, url, params } = sync
   next(others)
   let eventName = api[url]
   let ktm = Date.now()
-  console.log("send event: ", eventName, " params: ",JSON.stringify(params))
+  console.log("send event: ", eventName)
+  console.log("params: ", params)
   ipcRenderer.send(eventName, params, ktm)
   return new Promise( (resolve, reject) => {
     return ipcRenderer.once(`${eventName}Response?ktm=${ktm}`, (event, arg) => {
-      //console.log(`response event: ${eventName}Response?ktm=${ktm}, data: ${JSON.stringify(arg)}`)
+      console.log(`response event: ${eventName}Response?ktm=${ktm}`)
+      console.log('data', arg)
       resolve(arg)
     }) 
   })
